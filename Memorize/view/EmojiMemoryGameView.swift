e8b6d3366ed1b281d591784938965818bf59a2ae
Emily Thorne <53380331+sssaucie@users.noreply.github.com>
2021-07-06 13:01:05 -0600

    emoji size
    fixing emoji size by adding geometry to view

--- Memorize/view/EmojiMemoryGameView.swift
+++ Memorize/view/EmojiMemoryGameView.swift
@@ -13,7 +13,7 @@ struct EmojiMemoryGameView: View {
     
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
                 ForEach(gameViewModel.cards) { card in
                     CardView(card: card)
                         .aspectRatio(2/3, contentMode: .fit)
@@ -31,22 +31,32 @@ struct EmojiMemoryGameView: View {
         let card: EmojiMemoryGame.Card
         
         var body: some View {
-            ZStack {
-                let shape = RoundedRectangle(cornerRadius: 20)
-                if card.isFaceUp {
-                    shape.fill().foregroundColor(.white)
-                    shape.strokeBorder(lineWidth: 3)
-                    Text(verbatim: card.content).font(.largeTitle)
-                } else if card.isMatched {
-                    shape.opacity(0)
-                } else {
-                    shape.fill()
+            GeometryReader{ geometry in
+                ZStack {
+                    let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+                    if card.isFaceUp {
+                        shape.fill().foregroundColor(.white)
+                        shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                        Text(card.content).font(font(in: geometry.size))
+                    } else if card.isMatched {
+                        shape.opacity(0)
+                    } else {
+                        shape.fill()
+                    }
                 }
             }
         }
+        private func font(in size: CGSize) -> Font {
+            Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+        }
+        private struct DrawingConstants {
+            static let cornerRadius: CGFloat = 20
+            static let lineWidth: CGFloat = 3
+            static let fontScale: CGFloat = 0.75
+        }
     }
     
-    
+
     // Automated code to populate view for our preview
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
