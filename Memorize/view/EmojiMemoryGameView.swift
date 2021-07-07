f5d5eee64b5b8c4521c0e06feb4af8e6a96359b6
Emily Thorne <53380331+sssaucie@users.noreply.github.com>
2021-07-07 12:52:29 -0600

    animations
    Creating animations on the cards

--- Memorize/view/EmojiMemoryGameView.swift
+++ Memorize/view/EmojiMemoryGameView.swift
@@ -12,19 +12,36 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var gameViewModel: EmojiMemoryGame
     
     var body: some View {
+        VStack {
+            gameBody
+            shuffle
+        }
+        .padding()
+    }
+    
+    var gameBody: some View {
         AspectVGrid(items: gameViewModel.cards, aspectRatio: 2/3) { card in
             if card.isMatched && !card.isFaceUp {
-                Rectangle().opacity(0)
+                Color.clear
             } else {
-            CardView(card: card)
-                .padding(4)
-                .onTapGesture {
-                    gameViewModel.choose(card)
-                }
+                CardView(card: card)
+                    .padding(4)
+                    .onTapGesture {
+                        withAnimation {
+                            gameViewModel.choose(card)
+                        }
+                    }
             }
         }
         .foregroundColor(.red)
-        .padding(.horizontal)
+    }
+    
+    var shuffle: some View {
+        Button("Shuffle") {
+            withAnimation {
+                gameViewModel.shuffle()
+            }
+        }
     }
     
     struct CardView: View {
@@ -33,32 +50,30 @@ struct EmojiMemoryGameView: View {
         var body: some View {
             GeometryReader{ geometry in
                 ZStack {
-                    let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-                    if card.isFaceUp {
-                        shape.fill().foregroundColor(.white)
-                        shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-                        Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90))
-                        .padding(5).opacity(0.5)
-                        Text(card.content).font(font(in: geometry.size))
-                    } else if card.isMatched {
-                        shape.opacity(0)
-                    } else {
-                        shape.fill()
-                    }
+                    Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90))
+                        .padding(5)
+                        .opacity(0.5)
+                    Text(card.content)
+                        .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                        .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                        .font(Font.system(size: DrawingConstants.fontSize))
+                        .scaleEffect(scale(thatFits: geometry.size))
                 }
+                .cardify(isFaceUp: card.isFaceUp)
             }
         }
-        private func font(in size: CGSize) -> Font {
-            Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+        
+        private func scale(thatFits size: CGSize) -> CGFloat {
+            min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
         }
+        
         private struct DrawingConstants {
-            static let cornerRadius: CGFloat = 10
-            static let lineWidth: CGFloat = 3
             static let fontScale: CGFloat = 0.7
+            static let fontSize: CGFloat = 30
         }
     }
 }
-    
+
 // Automated code to populate view for our preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -68,4 +83,3 @@ struct ContentView_Previews: PreviewProvider {
             .preferredColorScheme(.dark)
     }
 }
-
